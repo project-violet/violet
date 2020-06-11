fbbaa55b4bda495ce5deeb52e0e9da67e724f3e0
violet-dev <violet.dev.master@gmail.com>
2020-06-11 23:35:42 +0900

    (ttt) Add some code

---
+++ app/lib/pages/license_page.dart
@@ -0,0 +1,15 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2020. violet-team. Licensed under the MIT Licence.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:tuple/tuple.dart';
+
+class LicensePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Markdown(data: 'https://raw.githubusercontent.com/project-violet/violet-public/master/LICENSE.md')
+    );
+  }
+}
