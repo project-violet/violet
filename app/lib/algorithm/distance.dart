b6bcfbde23aed2cc7138ac74942bf79b1a46d7c6
violet-dev <violet.dev.master@gmail.com>
2020-06-13 13:41:57 +0900

    (algorithm) Implements title clustring method

--- app/lib/algorithm/distance.dart
+++ app/lib/algorithm/distance.dart
@@ -35,4 +35,25 @@ class Distance {
 
     return v0[y + y + 1];
   }
+  
+  static int levenshteinDistanceComparable<T extends Comparable<T>>(List<T> l1, List<T> l2) {
+    int x = l1.length;
+    int y = l2.length;
+    int i, j;
+
+    if (x == 0) return x;
+    if (y == 0) return y;
+    var v0 = List<int>((y + 1) << 1);
+
+    for (i = 0; i < y + 1; i++) v0[i] = i;
+    for (i = 0; i < x; i++) {
+      v0[y + 1] = i + 1;
+      for (j = 0; j < y; j++)
+        v0[y + j + 2] = min(min(v0[y + j + 1], v0[j + 1]) + 1,
+            v0[j] + ((l1[i] == l2[j]) ? 0 : 1));
+      for (j = 0; j < y + 1; j++) v0[j] = v0[y + j + 1];
+    }
+
+    return v0[y + y + 1];
+  }
 }
