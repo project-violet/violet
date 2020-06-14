dae6358dd635e6ca275f4dc72b25be263fedae64
violet-dev <violet.dev.master@gmail.com>
2020-06-14 23:04:55 +0900

    (shit) Implements some shits

---
+++ app/lib/shielder.dart
@@ -0,0 +1,50 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2020. violet-team. Licensed under the MIT License.
+
+// Pornography is illegal in South Korea.
+// Also, in accordance with the Child and Youth Protection Act, 
+// just watching a cartoon with a 2d character can get the same punishment 
+// as having a child pornography if it is a character related to a child.
+// The law was created a long time ago and the amendment came into effect on June 2, 2020. 
+// The amendments became stronger, and controls and regulations became tighter.
+// It sounds like a ridiculous word, but in Korea, it is all true.
+// 
+// Action for Koreans
+// 1. Filtering related tags.
+// 2. Do not caching images
+// 3. Delegating responsibility to users
+//
+// I am not a Korean, and I do not have a Korean nationality and 
+// I live in a country not subject to Korean Law.
+class KoreanShielderFilter {
+
+  static List<String> tags = [
+    "female:loli",
+    "female:low_lolicon",
+    "female:oppai_loli",
+    "female:gothic_lolita",
+    "male:shota",
+    "male:yaoi",
+    "male:tomgirl",
+    "male:furry",
+    "male:low_shotacon",
+    "female:schoolgirl_uniform",
+    "male:schoolboy_uniform",
+    "female:school_swimsuit",
+    "female:schoolgirl",
+    "female:schoolboy",
+    "female:school_uniform",
+    "male:schoolgirl",
+    "male:schoolgirl_uniform",
+    "male:schoolboy",
+    "male:school_swimsuit",
+    "female:schoolboy_uniform",
+    "female:toddlercon",
+    "male:toddlercon",
+    "female:kindergarten_uniform",
+    "male:kindergarten_uniform",
+    "female:randoseru",
+    "male:randoseru",
+  ];
+
+}
