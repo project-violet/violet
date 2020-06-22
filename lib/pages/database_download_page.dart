6e66899c0e6f853ad82cb8b502d1f292ceccebb3
violet-dev <violet.dev.master@gmail.com>
2020-06-22 10:09:12 +0900

    (search) Implement backspace and space button

--- lib/pages/database_download_page.dart
+++ lib/pages/database_download_page.dart
@@ -34,7 +34,7 @@ class DataBaseDownloadPage extends StatefulWidget {
 
 class DataBaseDownloadPagepState extends State<DataBaseDownloadPage> {
   final imgUrl =
-      "https://github.com/iiccpp/downloader/releases/download/base_database1/hitomidata.db";
+      "https://github.com/violet-dev/db/releases/download/2020.06.20/hitomidata.db";
   bool downloading = false;
   var baseString = "요청을 기다리는 중...";
   var progressString = "";
