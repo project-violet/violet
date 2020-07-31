f234c21e84ecea2611e64d0ef791e105f85cc4fc
violet-dev <violet.dev.master@gmail.com>
2020-07-31 23:12:01 +0900

    (version) Update version

--- lib/update_sync.dart
+++ lib/update_sync.dart
@@ -15,7 +15,7 @@ class UpdateSyncManager {
   // Current version
   static const int majorVersion = 0;
   static const int minorVersion = 8;
-  static const int patchVersion = 3;
+  static const int patchVersion = 4;
 
   static bool updateRequire = false;
   static String version = "";
