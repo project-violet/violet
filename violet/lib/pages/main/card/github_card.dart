661f4c286f19eb0a15cf5f9f61d549bc700a9716
violet-dev <violet.dev.master@gmail.com>
2020-08-02 17:08:23 +0900

    (main) Split cards

---
+++ violet/lib/pages/main/card/github_card.dart
@@ -0,0 +1,78 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2020. violet-team. Licensed under the MIT License.
+
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:violet/locale/locale.dart';
+
+class GithubCard extends StatefulWidget {
+  @override
+  _GithubCardState createState() => _GithubCardState();
+}
+
+class _GithubCardState extends State<GithubCard> {
+  bool pressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    return SizedBox(
+      width: width - 16,
+      height: 65,
+      child: Parent(
+        style: settingsItemStyle(pressed),
+        gesture: Gestures()
+          ..isTap((isTapped) {
+            setState(() => pressed = isTapped);
+          })
+          ..onTapUp((detail) async {
+            const url = 'https://github.com/project-violet/';
+            if (await canLaunch(url)) {
+              await launch(url);
+            }
+          }),
+        child: Container(
+          width: width - 16,
+          height: 65,
+          margin: EdgeInsets.symmetric(horizontal: 0.0),
+          child: Card(
+            color: Color(0xFF24292E),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  MdiIcons.github,
+                  color: Colors.white,
+                ),
+                Padding(
+                  padding: Translations.of(context).dbLanguageCode == 'en'
+                      ? EdgeInsets.only(top: 4)
+                      : EdgeInsets.only(top: 4),
+                  child: Text(
+                    '  ${Translations.of(context).trans('maingithub')}',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: "Calibre-Semibold",
+                      letterSpacing: 0.5,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  final settingsItemStyle = (pressed) => ParentStyle()
+    ..elevation(pressed ? 0 : 10000, color: Colors.transparent)
+    ..scale(pressed ? 0.95 : 1.0)
+    ..alignmentContent.center()
+    ..ripple(true)
+    ..animate(150, Curves.easeOut);
+}
