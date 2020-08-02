ae67af9d53c9c7c96b04bfd1d004b3b13fac3b38
violet-dev <violet.dev.master@gmail.com>
2020-08-02 13:08:41 +0900

    (main) Tidy main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -41,13 +41,6 @@ Future<void> warmupFlare() async {
   }
 }
 
-Future<void> initDB() async {
-  await Bookmark.getInstance();
-  await User.getInstance();
-}
-
-FirebaseAnalytics analytics;
-FirebaseAnalyticsObserver observer;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterDownloader.initialize();
@@ -56,8 +49,8 @@ void main() async {
   Crashlytics.instance.enableInDevMode = true;
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
-  analytics = FirebaseAnalytics();
-  observer = FirebaseAnalyticsObserver(analytics: analytics);
+  var analytics = FirebaseAnalytics();
+  var observer = FirebaseAnalyticsObserver(analytics: analytics);
   var id = (await SharedPreferences.getInstance()).getString('fa_userid');
   if (id == null) {
     var ii = sha1.convert(utf8.encode(DateTime.now().toString()));
@@ -67,109 +60,109 @@ void main() async {
   await analytics.setUserId(id);
 
   await Settings.init();
-  await initDB();
+  await Bookmark.getInstance();
+  await User.getInstance();
   await Variables.init();
   await HitomiIndexs.init();
+  await warmupFlare();
 
-  warmupFlare().then((_) {
-    runApp(
-      DynamicTheme(
-        defaultBrightness: Brightness.light,
-        data: (brightness) => new ThemeData(
-          accentColor: Settings.majorColor,
-          // primaryColor: Settings.majorColor,
-          // primarySwatch: Settings.majorColor,
-          brightness: brightness,
-        ),
-        themedWidgetBuilder: (context, theme) {
-          return MaterialApp(
-            navigatorObservers: [
-              FirebaseAnalyticsObserver(analytics: analytics),
-            ],
-            theme: theme,
-            home: SplashPage(),
-            supportedLocales: [
-              const Locale('en', 'US'),
-              const Locale('ko', 'KR'),
-              const Locale('ja', 'JP'),
-              const Locale('zh', 'CH'),
-              const Locale('it', 'IT'),
-              const Locale('eo', 'ES'),
-            ],
-            routes: <String, WidgetBuilder>{
-              '/AfterLoading': (BuildContext context) => WillPopScope(
-                    child: new AfterLoadingPage(),
-                    onWillPop: () {
-                      DateTime now = DateTime.now();
-                      if (currentBackPressTime == null ||
-                          now.difference(currentBackPressTime) >
-                              Duration(seconds: 2)) {
-                        currentBackPressTime = now;
-                        scaffoldKey.currentState.showSnackBar(new SnackBar(
-                          duration: Duration(seconds: 2),
-                          content: new Text(
-                            Translations.of(context).trans('closedoubletap'),
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          backgroundColor: Colors.grey.shade800,
-                        ));
-                        return Future.value(false);
-                      }
-                      return Future.value(true);
-                    },
-                  ),
-              '/DatabaseDownload': (BuildContext context) =>
-                  new DataBaseDownloadPage(),
-            },
-            localizationsDelegates: [
-              const TranslationsDelegate(),
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate
-            ],
-            localeResolutionCallback:
-                (Locale locale, Iterable<Locale> supportedLocales) {
-              if (Settings.language != null) {
-                if (Settings.language.contains('_')) {
-                  var ss = Settings.language.split('_');
-                  if (ss.length == 2)
-                    return Locale.fromSubtags(
-                        languageCode: ss[0], scriptCode: ss[1]);
-                  else
-                    return Locale.fromSubtags(
-                        languageCode: ss[0],
-                        scriptCode: ss[1],
-                        countryCode: ss[2]);
-                } else
-                  return Locale(Settings.language);
-              }
-
-              if (locale == null) {
-                debugPrint("*language locale is null!!!");
-                if (Settings.language == null) {
-                  Settings.setLanguage(supportedLocales.first.languageCode);
-                }
-                return supportedLocales.first;
-              }
-
-              for (Locale supportedLocale in supportedLocales) {
-                if (supportedLocale.languageCode == locale.languageCode ||
-                    supportedLocale.countryCode == locale.countryCode) {
-                  debugPrint("*language ok $supportedLocale");
-                  if (Settings.language == null) {
-                    Settings.setLanguage(supportedLocale.languageCode);
-                  }
-                  return supportedLocale;
-                }
-              }
-
-              debugPrint("*language to fallback ${supportedLocales.first}");
-              if (Settings.language == null)
-                Settings.setLanguage(supportedLocales.first.languageCode);
-              return supportedLocales.first;
-            },
-          );
-        },
+  runApp(
+    DynamicTheme(
+      defaultBrightness: Brightness.light,
+      data: (brightness) => new ThemeData(
+        accentColor: Settings.majorColor,
+        // primaryColor: Settings.majorColor,
+        // primarySwatch: Settings.majorColor,
+        brightness: brightness,
       ),
-    );
-  });
+      themedWidgetBuilder: (context, theme) {
+        return MaterialApp(
+          navigatorObservers: [
+            FirebaseAnalyticsObserver(analytics: analytics),
+          ],
+          theme: theme,
+          home: SplashPage(),
+          supportedLocales: [
+            const Locale('en', 'US'),
+            const Locale('ko', 'KR'),
+            const Locale('ja', 'JP'),
+            const Locale('zh', 'CH'),
+            const Locale('it', 'IT'),
+            const Locale('eo', 'ES'),
+          ],
+          routes: <String, WidgetBuilder>{
+            '/AfterLoading': (BuildContext context) => WillPopScope(
+                  child: new AfterLoadingPage(),
+                  onWillPop: () {
+                    DateTime now = DateTime.now();
+                    if (currentBackPressTime == null ||
+                        now.difference(currentBackPressTime) >
+                            Duration(seconds: 2)) {
+                      currentBackPressTime = now;
+                      scaffoldKey.currentState.showSnackBar(new SnackBar(
+                        duration: Duration(seconds: 2),
+                        content: new Text(
+                          Translations.of(context).trans('closedoubletap'),
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        backgroundColor: Colors.grey.shade800,
+                      ));
+                      return Future.value(false);
+                    }
+                    return Future.value(true);
+                  },
+                ),
+            '/DatabaseDownload': (BuildContext context) =>
+                new DataBaseDownloadPage(),
+          },
+          localizationsDelegates: [
+            const TranslationsDelegate(),
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate
+          ],
+          localeResolutionCallback:
+              (Locale locale, Iterable<Locale> supportedLocales) {
+            if (Settings.language != null) {
+              if (Settings.language.contains('_')) {
+                var ss = Settings.language.split('_');
+                if (ss.length == 2)
+                  return Locale.fromSubtags(
+                      languageCode: ss[0], scriptCode: ss[1]);
+                else
+                  return Locale.fromSubtags(
+                      languageCode: ss[0],
+                      scriptCode: ss[1],
+                      countryCode: ss[2]);
+              } else
+                return Locale(Settings.language);
+            }
+
+            if (locale == null) {
+              debugPrint("*language locale is null!!!");
+              if (Settings.language == null) {
+                Settings.setLanguage(supportedLocales.first.languageCode);
+              }
+              return supportedLocales.first;
+            }
+
+            for (Locale supportedLocale in supportedLocales) {
+              if (supportedLocale.languageCode == locale.languageCode ||
+                  supportedLocale.countryCode == locale.countryCode) {
+                debugPrint("*language ok $supportedLocale");
+                if (Settings.language == null) {
+                  Settings.setLanguage(supportedLocale.languageCode);
+                }
+                return supportedLocale;
+              }
+            }
+
+            debugPrint("*language to fallback ${supportedLocales.first}");
+            if (Settings.language == null)
+              Settings.setLanguage(supportedLocales.first.languageCode);
+            return supportedLocales.first;
+          },
+        );
+      },
+    ),
+  );
 }
