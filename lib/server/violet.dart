12253cad8e91434c7d69d94d6994a88b27d18820
violet-dev <violet.dev.master@gmail.com>
2020-09-07 15:50:01 +0900

    (server) Add violet server [1]

---
+++ lib/server/violet.dart
@@ -0,0 +1,28 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2020. violet-team. Licensed under the Apache-2.0 License.
+
+import 'dart:convert';
+
+import 'package:tuple/tuple.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:violet/server/salt.dart' as salt;
+
+class VioletServer {
+  static const protocol = 'https';
+  static const host = 'api.koromo.xyz';
+  static const api = '$protocol://$host';
+
+  static Future<List<Tuple2<int, int>>> top(
+      int offset, int count, String type) async {
+    var gg = await http.get('$api/top?offset=$offset&count=$count&type=$type');
+
+    if (gg.statusCode != 200) {
+      return null;
+    }
+
+    return (jsonDecode(gg.body)['result'] as List<dynamic>).map((e) =>
+        Tuple2<int, int>(
+            (e as List<dynamic>)[0] as int, (e as List<dynamic>)[1] as int));
+  }
+}
