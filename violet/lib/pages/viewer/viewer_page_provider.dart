7657ae1e0438b9ca5427e024c475ced07f83f67b
violet-dev <violet.dev.master@gmail.com>
2020-09-10 21:28:51 +0900

    (viewer) Add page provider

---
+++ violet/lib/pages/viewer/viewer_page_provider.dart
@@ -0,0 +1,18 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2020. violet-team. Licensed under the Apache-2.0 License.
+
+import 'package:flutter/cupertino.dart';
+
+class ViewerPageProvider {
+  final bool useFileSystem;
+  final bool useWeb;
+  final List<String> uris;
+  final Map<String, String> headers;
+
+  ViewerPageProvider({
+    this.useFileSystem = false,
+    this.useWeb = false,
+    @required this.uris,
+    this.headers,
+  });
+}
