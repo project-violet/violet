89b714919b839741f44db0971f9d7754cc30196b
violet-dev <violet.dev.master@gmail.com>
2020-09-20 19:52:17 +0900

    (hitomi/downloader) Download without database

--- lib/component/download/hitomi.dart
+++ lib/component/download/hitomi.dart
@@ -1,7 +1,6 @@
 // This source code is a part of Project Violet.
 // Copyright (C) 2020. violet-team. Licensed under the Apache-2.0 License.
 
-// Reference https://github.com/rollrat/downloader/blob/master/Koromo_Copy.Framework/Extractor/PixivExtractor.cs
 import 'dart:io';
 
 import 'package:html_unescape/html_unescape_small.dart';
@@ -13,6 +12,7 @@ import 'dart:convert';
 import 'package:path/path.dart' as path;
 import 'package:crypto/crypto.dart';
 import 'package:violet/component/hitomi/hitomi.dart';
+import 'package:violet/component/hitomi/hitomi_parser.dart';
 import 'package:violet/database/database.dart';
 import 'package:violet/database/query.dart';
 
@@ -31,7 +31,7 @@ class HitomiDonwloadManager extends Downloadable {
 
   @override
   String defaultFormat() {
-    return "%(extractor)s/%(artist)s/[%(id)s] %(title)s/%(file)s.%(ext)s";
+    return "%(extractor)s/[%(id)s] %(title)s/%(file)s.%(ext)s";
   }
 
   @override
@@ -73,38 +73,66 @@ class HitomiDonwloadManager extends Downloadable {
         .map((e) => QueryResult(result: e))
         .toList();
 
-    if (articles == null || articles.length == 0) {
-      return null;
-    }
-
-    var article = articles[0];
-
-    gdp.simpleInfoCallback('[$id] ${article.title()}');
-
-    var images = await HitomiManager.getImageList(id);
     var result = List<DownloadTask>();
+    if (articles != null && articles.length != 0) {
+      var article = articles[0];
 
-    gdp.thumbnailCallback(images.item2[0],
-        jsonEncode({'Referer': 'https://hitomi.la/reader/$id.html'}));
+      gdp.simpleInfoCallback('[$id] ${article.title()}');
 
-    for (int i = 0; i < images.item1.length; i++) {
-      var img = images.item1[i];
-      result.add(
-        DownloadTask(
-          url: img,
-          filename: img.split('/').last,
-          referer: 'https://hitomi.la/reader/$id.html',
-          format: FileNameFormat(
-            title: article.title(),
-            id: id,
-            laugage: article.language(),
-            uploadDate: article.getDateTime().toString(),
-            filenameWithoutExtension: intToString(i, pad: 3),
-            extension: path.extension(img.split('/').last).replaceAll(".", ""),
-            extractor: 'hitomi',
+      var images = await HitomiManager.getImageList(id);
+
+      gdp.thumbnailCallback(images.item2[0],
+          jsonEncode({'Referer': 'https://hitomi.la/reader/$id.html'}));
+
+      for (int i = 0; i < images.item1.length; i++) {
+        var img = images.item1[i];
+        result.add(
+          DownloadTask(
+            url: img,
+            filename: img.split('/').last,
+            referer: 'https://hitomi.la/reader/$id.html',
+            format: FileNameFormat(
+              title: article.title(),
+              id: id,
+              laugage: article.language(),
+              uploadDate: article.getDateTime().toString(),
+              filenameWithoutExtension: intToString(i, pad: 3),
+              extension:
+                  path.extension(img.split('/').last).replaceAll(".", ""),
+              extractor: 'hitomi',
+            ),
           ),
-        ),
-      );
+        );
+      }
+    } else {
+      var html = await http.get('https://ltn.hitomi.la/galleryblock/$id.html');
+      var title = await HitomiParser.parseGalleryBlock(html.body);
+
+      gdp.simpleInfoCallback('[$id] $title');
+
+      var images = await HitomiManager.getImageList(id);
+
+      gdp.thumbnailCallback(images.item2[0],
+          jsonEncode({'Referer': 'https://hitomi.la/reader/$id.html'}));
+
+      for (int i = 0; i < images.item1.length; i++) {
+        var img = images.item1[i];
+        result.add(
+          DownloadTask(
+            url: img,
+            filename: img.split('/').last,
+            referer: 'https://hitomi.la/reader/$id.html',
+            format: FileNameFormat(
+              title: title,
+              id: id,
+              filenameWithoutExtension: intToString(i, pad: 3),
+              extension:
+                  path.extension(img.split('/').last).replaceAll(".", ""),
+              extractor: 'hitomi',
+            ),
+          ),
+        );
+      }
     }
 
     return result;
