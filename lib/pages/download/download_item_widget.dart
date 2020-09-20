f6e2a99b9279f5d76a80048becc3e5b9b7cee3d3
violet-dev <violet.dev.master@gmail.com>
2020-09-20 19:11:18 +0900

    (downloader) Show downloaded images

--- lib/pages/download/download_item_widget.dart
+++ lib/pages/download/download_item_widget.dart
@@ -17,6 +17,8 @@ import 'package:violet/component/downloadable.dart' as violetd;
 import 'package:violet/locale/locale.dart';
 import 'package:violet/pages/download/download_item_menu.dart';
 import 'package:violet/downloader/native_downloader.dart';
+import 'package:violet/pages/download/gallery/gallery_item.dart';
+import 'package:violet/pages/download/gallery/gallery_page.dart';
 import 'package:violet/settings/settings.dart';
 import 'package:violet/database/user/download.dart';
 import 'package:violet/widgets/toast.dart';
@@ -310,6 +312,33 @@ class _DownloadItemWidgetState extends State<DownloadItemWidget>
           widget.download = true;
           _downloadProcedure();
           setState(() {});
+        }
+      },
+      onTap: () {
+        if (widget.item.state() == 0 && widget.item.files() != null) {
+          var gi = GalleryItem.fromDonwloadItem(widget.item);
+
+          if (gi.length != 0) {
+            Navigator.of(context).push(PageRouteBuilder(
+              transitionDuration: Duration(milliseconds: 500),
+              transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
+                var begin = Offset(0.0, 1.0);
+                var end = Offset.zero;
+                var curve = Curves.ease;
+
+                var tween = Tween(begin: begin, end: end)
+                    .chain(CurveTween(curve: curve));
+
+                return SlideTransition(
+                  position: animation.drive(tween),
+                  child: child,
+                );
+              },
+              pageBuilder: (_, __, ___) =>
+                  GalleryPage(item: gi, model: widget.item),
+            ));
+          }
         }
       },
       onTapDown: (details) {
