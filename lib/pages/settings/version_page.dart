656f929a4ff81b2bf62529562c894a4d133a02fc
violet-dev <violet.dev.master@gmail.com>
2020-09-21 19:54:38 +0900

    (version) Update version

--- lib/pages/settings/version_page.dart
+++ lib/pages/settings/version_page.dart
@@ -29,7 +29,7 @@ class VersionViewPage extends StatelessWidget {
                       style: TextStyle(fontSize: 30),
                     ),
                     Text(
-                      '1.3.0',
+                      '1.3.1',
                       style: TextStyle(fontSize: 20),
                     ),
                     Text(''),
