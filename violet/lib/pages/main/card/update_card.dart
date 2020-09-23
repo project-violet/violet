a476b232ddeaaa6afe703224d59e6cc0edbb561c
violet-dev <violet.dev.master@gmail.com>
2020-09-23 16:00:19 +0900

    (db) Fix indexing file name

--- violet/lib/pages/main/card/update_card.dart
+++ violet/lib/pages/main/card/update_card.dart
@@ -8,7 +8,6 @@ import 'dart:ui';
 import 'package:division/division.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:gradient_widgets/gradient_widgets.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:path_provider/path_provider.dart';
