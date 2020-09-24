5fa2ff2ea69734a4a92d7e349d00f9d6e1596b07
violet-dev <violet.dev.master@gmail.com>
2020-09-25 00:46:05 +0900

    (hentai) Implement hitomi/hiyobi/ehentai/exhentai image provider

---
+++ lib/component/hitomi/hitomi_provider.dart
@@ -0,0 +1,36 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2020. violet-team. Licensed under the Apache-2.0 License.
+
+import 'package:violet/component/image_provider.dart';
+import 'package:tuple/tuple.dart';
+
+class HitomiImageProvider extends VioletImageProvider {
+  Tuple3<List<String>, List<String>, List<String>> urls;
+
+  HitomiImageProvider(this.urls);
+
+  @override
+  Future<List<String>> getSmallImagesUrl() async {
+    return urls.item3;
+  }
+
+  @override
+  Future<String> getThumbnailUrl() async {
+    return urls.item2[0];
+  }
+
+  @override
+  Future<Map<String, String>> getHeader(int page) async {
+    return {"Referer": 'https://hitomi.la/reader/1234.html'};
+  }
+
+  @override
+  Future<String> getImageUrl(int page) async {
+    return urls.item1[page];
+  }
+
+  @override
+  int length() {
+    return urls.item1.length;
+  }
+}
