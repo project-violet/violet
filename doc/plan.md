e64c02fd6dd4a7fcb1a8c40a4bffcdac08c28af8
violet-dev <violet.dev.master@gmail.com>
2020-09-24 19:31:39 +0900

    (afterloading) Implements drawer

---
+++ doc/plan.md
@@ -0,0 +1,3 @@
+# Plan
+
+## Support Tablet
--- lib/pages/after_loading/afterloading_page.dart
+++ lib/pages/after_loading/afterloading_page.dart
@@ -14,6 +14,7 @@ import 'package:violet/pages/main/main_page.dart';
 import 'package:violet/pages/search/search_page.dart';
 import 'package:violet/pages/settings/settings_page.dart';
 import 'package:violet/settings/settings.dart';
+import 'package:violet/version/update_sync.dart';
 
 class AfterLoadingPage extends StatefulWidget {
   @override
@@ -54,6 +55,13 @@ class _AfterLoadingPageState extends State<AfterLoadingPage>
 
   @override
   Widget build(BuildContext context) {
+    if (!Settings.useDrawer)
+      return _bottomNav();
+    else
+      return _drawer();
+  }
+
+  _bottomNav() {
     final mediaQuery = MediaQuery.of(context);
     if (Platform.isAndroid) {
       return new Scaffold(
@@ -186,7 +194,160 @@ class _AfterLoadingPageState extends State<AfterLoadingPage>
         // ),
       );
     }
-
     throw new Exception('not implemented');
+  }
+
+  int page = 0;
+  _drawer() {
+    final mediaQuery = MediaQuery.of(context);
+    if (Platform.isAndroid) {
+      return new Scaffold(
+        body: PageView(
+          physics: new NeverScrollableScrollPhysics(),
+          controller: _c,
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            MainPage(),
+            SearchPage(),
+            BookmarkPage(),
+            DownloadPage(),
+            SettingsPage(),
+          ],
+        ),
+        drawer: Container(
+          width: 200,
+          padding: mediaQuery.padding + mediaQuery.viewInsets,
+          child: Drawer(
+            child: Column(
+              children: <Widget>[
+                // DrawerHeader(
+                //   child: Text('Drawer Header'),
+                //   decoration: BoxDecoration(
+                //     color: Colors.blue,
+                //   ),
+                // ),
+                Container(
+                  margin: EdgeInsets.all(40),
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        InkWell(
+                          child: Image.asset(
+                            'assets/images/logo.png',
+                            width: 100,
+                            height: 100,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 12),
+                        ),
+                        Text(
+                          'Project Violet',
+                          style: TextStyle(
+                            color: Settings.themeWhat
+                                ? Colors.white
+                                : Colors.black87,
+                            fontSize: 16.0,
+                            fontFamily: "Calibre-Semibold",
+                            letterSpacing: 1.0,
+                          ),
+                        ),
+                        Text(
+                            '${UpdateSyncManager.majorVersion}.${UpdateSyncManager.minorVersion}.${UpdateSyncManager.patchVersion}',
+                            style: TextStyle(
+                                fontFamily: "Calibre-Semibold", fontSize: 18))
+                      ],
+                    ),
+                  ),
+                ),
+                _drawerButton(
+                    MdiIcons.home,
+                    0,
+                    Translations.of(context).trans('main'),
+                    Settings.majorColor),
+                _drawerButton(
+                    Icons.search,
+                    1,
+                    Translations.of(context).trans('search'),
+                    Settings.majorColor),
+                _drawerButton(
+                    MdiIcons.bookmark,
+                    2,
+                    Translations.of(context).trans('bookmark'),
+                    Settings.majorColor),
+                _drawerButton(
+                    MdiIcons.download,
+                    3,
+                    Translations.of(context).trans('download'),
+                    Settings.majorColor),
+                _drawerButton(
+                    Icons.settings,
+                    4,
+                    Translations.of(context).trans('settings'),
+                    Settings.majorColor),
+                Expanded(
+                    child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    'Copyright (C) 2020\nby project-violet',
+                    style: TextStyle(
+                      color: Settings.themeWhat ? Colors.white : Colors.black87,
+                      fontSize: 14.0,
+                      fontFamily: "Calibre-Semibold",
+                      letterSpacing: 1.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                )),
+                Container(height: 16)
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+  }
+
+  _drawerButton(IconData icon, int page, String name, Color color) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(8, 4, 8, 4),
+      height: 54,
+      child: Container(
+        // color: ,
+        decoration: BoxDecoration(
+            color: this.page == page ? color.withOpacity(0.4) : null,
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: InkWell(
+          customBorder: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          hoverColor: color,
+          highlightColor: color.withOpacity(0.2),
+          focusColor: color,
+          splashColor: color.withOpacity(0.3),
+          child: Row(
+            children: [
+              Container(width: 12),
+              Icon(icon),
+              Container(width: 12),
+              Text(
+                name,
+                style: TextStyle(fontWeight: FontWeight.bold),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+          ),
+          onTap: () {
+            this.page = page;
+            Navigator.pop(context);
+            setState(() {});
+            // this._c.animateToPage(page,
+            //     duration: const Duration(milliseconds: 250),
+            //     curve: Curves.easeInOut);
+            this._c.jumpToPage(page);
+          },
+        ),
+      ),
+    );
   }
 }
