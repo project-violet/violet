a4cbd7de58bdb6943c46ea96144a5bb15b2f0c34
violet-dev <violet.dev.master@gmail.com>
2020-09-30 09:49:46 +0900

    (tablet) Add device type sepecifier

---
+++ violet/lib/settings/device_type.dart
@@ -0,0 +1,57 @@
+// https://github.com/ominibyte/flutter_device_type/blob/master/lib/flutter_device_type.dart
+
+import 'dart:ui' as ui;
+import 'dart:io';
+
+class Device {
+  static double devicePixelRatio = ui.window.devicePixelRatio;
+  static ui.Size size = ui.window.physicalSize;
+  static double width = size.width;
+  static double height = size.height;
+  static double screenWidth = width / devicePixelRatio;
+  static double screenHeight = height / devicePixelRatio;
+  static ui.Size screenSize = new ui.Size(screenWidth, screenHeight);
+  final bool isTablet, isPhone, isIos, isAndroid, isIphoneX;
+  static Device _device;
+
+  Device(
+      {this.isTablet,
+      this.isPhone,
+      this.isIos,
+      this.isAndroid,
+      this.isIphoneX});
+
+  factory Device.get() {
+    if (_device != null) return _device;
+
+    bool isTablet;
+    bool isPhone;
+    bool isIos = Platform.isIOS;
+    bool isAndroid = Platform.isAndroid;
+    bool isIphoneX = false;
+
+    if (devicePixelRatio < 2 && (width >= 1000 || height >= 1000)) {
+      isTablet = true;
+      isPhone = false;
+    } else if (devicePixelRatio == 2 && (width >= 1920 || height >= 1920)) {
+      isTablet = true;
+      isPhone = false;
+    } else {
+      isTablet = false;
+      isPhone = true;
+    }
+
+    if (isIos &&
+        (screenHeight == 812 ||
+            screenWidth == 812 ||
+            screenHeight == 896 ||
+            screenWidth == 896)) isIphoneX = true;
+
+    return _device = new Device(
+        isTablet: isTablet,
+        isPhone: isPhone,
+        isAndroid: isAndroid,
+        isIos: isIos,
+        isIphoneX: isIphoneX);
+  }
+}
