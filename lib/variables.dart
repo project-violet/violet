3f8b48b697d000eee99bdc6019cc1adf25e18261
violet-dev <violet.dev.master@gmail.com>
2020-10-03 12:09:37 +0900

    (viewer) Change viewer theme

--- lib/variables.dart
+++ lib/variables.dart
@@ -1,6 +1,8 @@
 // This source code is a part of Project Violet.
 // Copyright (C) 2020. violet-team. Licensed under the Apache-2.0 License.
 
+import 'dart:math';
+
 import 'package:path_provider/path_provider.dart';
 
 class Variables {
@@ -9,5 +11,12 @@ class Variables {
   static Future<void> init() async {
     applicationDocumentsDirectory =
         (await getApplicationDocumentsDirectory()).path;
+  }
+
+  static double statusBarHeight = 0;
+  static double bottomBarHeight = 0;
+  static void updatePadding(double statusBar, double bottomBar) {
+    statusBarHeight = max(statusBarHeight, statusBar);
+    bottomBarHeight = max(bottomBarHeight, bottomBar);
   }
 }
