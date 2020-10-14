ac0d94fea3c85c81e87dcbaada6a71be1135ed13
violet-dev <violet.dev.master@gmail.com>
2020-10-14 21:52:29 +0900

    (sync) Implements chunk sync

---
+++ lib/version/sync.dart
@@ -0,0 +1,101 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2020. violet-team. Licensed under the Apache-2.0 License.
+
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:violet/database/database.dart';
+import 'package:violet/database/query.dart';
+import 'package:violet/network/wrapper.dart' as http;
+
+class SyncInfoRecord {
+  final String type;
+  final int timestamp;
+  final String url;
+  final int size;
+
+  SyncInfoRecord({this.type, this.timestamp, this.url, this.size = 0});
+}
+
+class SyncManager {
+  static const String syncInfoURL = "https://koromo.xyz/version.txt";
+
+  static bool syncRequire = false;
+  static const ignoreUserAcceptThreshold = 1024 * 1024 * 10; // 10MB
+  static List<SyncInfoRecord> _rows;
+  static int requestSize = 0;
+
+  static Future<void> checkSync() async {
+    var ls = new LineSplitter();
+    var infoRaw = (await http.get(syncInfoURL)).body;
+    var lines = ls.convert(infoRaw);
+
+    var latest = (await SharedPreferences.getInstance()).getInt('synclatest');
+
+    if (latest == null) {
+      syncRequire = true;
+      latest = 0;
+    }
+
+    // lines: [old ... latest]
+    // _rows: [latest ... old]
+    _rows = List<SyncInfoRecord>();
+
+    lines.reversed.forEach((element) {
+      var split = element.split(' ');
+      var type = split[0];
+      var timestamp = int.parse(split[1]);
+      var url = split[2];
+      var size = 0;
+      if (type == 'chunk') size = int.parse(split[3]);
+
+      // We require only json files when synchronize with chunk.
+      if (type == 'chunk' && !url.endsWith('.json')) return;
+      if (timestamp <= latest) return;
+
+      requestSize += size;
+      _rows.add(SyncInfoRecord(
+          type: type, timestamp: timestamp, url: url, size: size));
+    });
+  }
+
+  static SyncInfoRecord getLatestDB() {
+    for (int i = 0; i < _rows.length; i++)
+      if (_rows[i].type == 'db') return _rows[i];
+
+    throw Exception('not reachable, check sync server');
+  }
+
+  static Future<void> doChunkSync() async {
+    for (int i = 0; i < _rows.length; i++) {
+      // Larger timestamp, the more recent data is contained.
+      // So, we need to update them in old order.
+      var row = _rows[_rows.length - i - 1];
+
+      // First, download json
+      var raw = (await http.get(row.url)).body;
+      var json = jsonDecode(raw);
+
+      // Second, convert json to query
+      var qlist = json as List<dynamic>;
+      var quries = qlist
+          .map((e) => QueryResult(result: e as Map<String, dynamic>))
+          .toList();
+
+      // Third, delete exists id
+      var db = await DataBaseManager.getInstance();
+      await db.delete('HitomiColumnModel',
+          'Id IN (${quries.map((e) => e.id() as String).join(',')})', []);
+
+      // Last, append datas
+      var dbraw = await openDatabase(db.dbPath);
+      for (int j = 0; j < quries.length; j++) {
+        // I think this is not a good way.
+        // I need to find a way to do all of the insert requests at once.
+        await dbraw.insert('HitomiColumnModel', quries[j].result);
+      }
+      await dbraw.close();
+    }
+  }
+}
