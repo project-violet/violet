9c4442a9fcbb5e14d2e9b56dfeefc76dc154887a
violet-dev <violet.dev.master@gmail.com>
2020-10-25 18:42:54 +0900

    (dev) Update emulator.bat

--- emulator.bat
+++ emulator.bat
@@ -1,2 +1,2 @@
 F:\android-sdk\emulator\emulator.exe -list-avds
-F:\android-sdk\emulator\emulator.exe -avd Pixel_2_XL_API_29
+F:\android-sdk\emulator\emulator.exe -avd Pixel_2_XL_API_29 -sdcard mySdCardFile.img
