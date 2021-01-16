58222db99f8a96cd0c017e6ad73c6f2ca9d169de
violet-dev <violet.dev.master@gmail.com>
2021-01-16 04:56:20 +0000

    (flutter) Update SliverPersistentHeaderDelegate overrides

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -39,4 +39,10 @@ class AnimatedOpacitySliver implements SliverPersistentHeaderDelegate {
 
   @override
   OverScrollHeaderStretchConfiguration get stretchConfiguration => null;
+
+  @override
+  PersistentHeaderShowOnScreenConfiguration get showOnScreenConfiguration => null;
+
+  @override
+  TickerProvider get vsync => null;
 }
