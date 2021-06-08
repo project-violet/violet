4e0836cae1df57926db021db8b354aee9b3def6e
violet-dev <violet.dev.master@gmail.com>
2021-06-08 03:21:47 +0000

    (viewer) Add user view tracker

---
+++ violet/lib/pages/viewer/viewer_report.dart
@@ -0,0 +1,31 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2021.violet-team. Licensed under the Apache-2.0 License.
+
+class ViewerReport {
+  final int id;
+  final int pages;
+  final DateTime startsTime;
+
+  ViewerReport({this.id, this.pages, this.startsTime});
+
+  int _lastPage;
+  set lastPage(int value) => _lastPage = value;
+  DateTime _endsTime;
+  set endsTime(DateTime value) => _endsTime = value;
+  int _validSeconds;
+  set validSeconds(int value) => _validSeconds = value;
+  List<int> _msPerPages;
+  set msPerPages(List<int> value) => _msPerPages = value; // unit of 100ms
+
+  dynamic submission() {
+    return {
+      'id': id,
+      'pages': pages,
+      'startsTime': startsTime.toUtc().millisecondsSinceEpoch,
+      'endsTime': _endsTime.toUtc().millisecondsSinceEpoch,
+      'lastPage': _lastPage,
+      'validSeconds': _validSeconds,
+      'msPerPages': _msPerPages,
+    };
+  }
+}
