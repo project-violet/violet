652daf7a3f2a01144a5c6616f404d1c76dcfa0f4
violet-dev <violet.dev.master@gmail.com>
2021-07-09 03:03:29 +0000

    (search-page) Apply search page 2

--- lib/pages/after_loading/afterloading_page.dart
+++ lib/pages/after_loading/afterloading_page.dart
@@ -10,7 +10,7 @@ import 'package:violet/other/named_color.dart';
 import 'package:violet/pages/bookmark/bookmark_page.dart';
 import 'package:violet/pages/download/download_page.dart';
 import 'package:violet/pages/main/main_page.dart';
-import 'package:violet/pages/search/search_page.dart';
+import 'package:violet/pages/search/search_page2.dart';
 import 'package:violet/pages/settings/settings_page.dart';
 import 'package:violet/settings/settings.dart';
 import 'package:violet/variables.dart';
