b927a52a33b92f4fc2f11f7246a9d341af07cf19
violet-dev <violet.dev.master@gmail.com>
2021-07-25 02:59:28 +0000

    (ldi) Implements ldi

--- violet/lib/component/hitomi/ldi.dart
+++ violet/lib/component/hitomi/ldi.dart
@@ -2,6 +2,7 @@
 // Copyright (C) 2020-2021.violet-team. Licensed under the Apache-2.0 License.
 
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:tuple/tuple.dart';
@@ -10,10 +11,18 @@ class LDI {
   static List<Tuple2<int, double>> ldi;
 
   static Future<void> init() async {
-    final data = await rootBundle.loadString('assets/rank/ldi.json');
+    String data;
+    if (Platform.environment.containsKey('FLUTTER_TEST')) {
+      var file = File('/home/ubuntu/violet/assets/rank/ldi.json');
+      data = await file.readAsString();
+    } else {
+      data = await rootBundle.loadString('assets/rank/ldi.json');
+    }
 
-    Map<int, dynamic> _ldi = json.decode(data);
-    ldi = _ldi.entries.map((x) => Tuple2<int, double>(x.key, x.value));
+    Map<String, dynamic> _ldi = json.decode(data);
+    ldi = _ldi.entries
+        .map((x) => Tuple2<int, double>(int.parse(x.key), x.value as double))
+        .toList();
     ldi.sort((x, y) => y.item2.compareTo(x.item2));
   }
 }
