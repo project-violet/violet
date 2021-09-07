841e10bfa72d4190ce9dc36bf33ef06888978a7f
violet-dev <violet.dev.master@gmail.com>
2021-09-07 09:37:56 +0000

    (package) Remove unused package

--- violet/lib/widgets/toast.dart
+++ violet/lib/widgets/toast.dart
@@ -7,8 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:violet/settings/settings.dart';
 import 'package:violet/variables.dart';
 
-import 'package:utopic_toast/utopic_toast.dart';
-
 class ToastWrapper extends StatefulWidget {
   final bool isCheck;
   final bool isWarning;
