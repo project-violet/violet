c2fe70415996dd0bd8c690bc61455037cac7f961
violet-dev <violet.dev.master@gmail.com>
2022-01-03 10:13:50 +0000

    script: request galleries with default headers

---
+++ violet/test/test_get.dart
@@ -0,0 +1,23 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2020-2022. violet-team. Licensed under the Apache-2.0 License.
+
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:violet/network/wrapper.dart';
+
+void main() {
+  setUp(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+  });
+
+  test("Test Bookmark", () async {
+    var res = await http
+        .get(Uri.parse('https://ltn.hitomi.la/galleries/2102839.js'), headers: {
+      'accept': HttpWrapper.accept,
+      'user-agent': HttpWrapper.mobileUserAgent,
+    });
+
+    print(res.body);
+  });
+}
