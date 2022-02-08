5c3c8771a1b56a58a2dae3336da2cf3b2819987a
violet-dev <violet.dev.master@gmail.com>
2022-02-08 03:15:52 +0000

    viewer: jump page when slider on chage end

--- violet/lib/pages/viewer/viewer_page.dart
+++ violet/lib/pages/viewer/viewer_page.dart
@@ -1742,8 +1742,6 @@ class _ViewerPageState extends State<ViewerPage>
                             },
                             onChangeEnd: (value) {
                               _sliderOnChange = false;
-                            },
-                            onChanged: (value) {
                               if (!Settings.isHorizontal) {
                                 itemScrollController.jumpTo(
                                     index: value.toInt() - 1, alignment: 0.12);
@@ -1755,6 +1753,7 @@ class _ViewerPageState extends State<ViewerPage>
                                 _prevPage = value.toInt();
                               });
                             },
+                            onChanged: (value) {},
                           ),
                         ),
                       ),
