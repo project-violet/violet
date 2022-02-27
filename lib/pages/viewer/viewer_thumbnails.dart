93b71e2343833c2819fddc9678ee3c95880cea80
violet-dev <violet.dev.master@gmail.com>
2022-02-27 01:30:31 +0000

    viewer-thumbanil: fix viewer provider url getter

--- lib/pages/viewer/viewer_thumbnails.dart
+++ lib/pages/viewer/viewer_thumbnails.dart
@@ -257,17 +257,19 @@ class _ViewerThumbnailState extends State<ViewerThumbnail> {
 
       infoText +=
           'type: ${_pageInfo.useProvider ? 'provider' : _pageInfo.useFileSystem ? 'filesys' : _pageInfo.useWeb ? 'web' : 'none'}\n';
-      infoText += 'raw-uri: ${_pageInfo.uris[i]}\n';
 
       if (_pageInfo.useProvider || _pageInfo.useFileSystem) {
         File file;
 
         if (_pageInfo.useProvider) {
+          final url = await _pageInfo.provider.getImageUrl(i);
           final headers = await _pageInfo.provider.getHeader(i);
+
+          infoText += 'url: $url\n';
           infoText += 'header: ${json.encode(headers)}\n';
 
-          file = await DefaultCacheManager()
-              .getSingleFile(_pageInfo.uris[i], headers: headers);
+          file =
+              await DefaultCacheManager().getSingleFile(url, headers: headers);
         } else if (_pageInfo.useFileSystem) {
           file = File(_pageInfo.uris[i]);
         }
