f9119b020060f1a2da4c9c4d9529c9287b4b9076
violet-dev <violet.dev.master@gmail.com>
2022-09-14 11:49:15 +0900

    viewer: add double viewer page prototype

---
+++ lib/pages/viewer/double_viewer_page.dart
@@ -0,0 +1,18 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2020-2022. violet-team. Licensed under the Apache-2.0 License.
+
+import 'package:flutter/material.dart';
+
+class DoubleViewerPage extends StatefulWidget {
+  const DoubleViewerPage({Key? key}) : super(key: key);
+
+  @override
+  State<DoubleViewerPage> createState() => _DoubleViewerPageState();
+}
+
+class _DoubleViewerPageState extends State<DoubleViewerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
