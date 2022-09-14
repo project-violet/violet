e616808d703c396bc9753d887a177496d41c1db4
violet-dev <violet.dev.master@gmail.com>
2022-09-14 19:01:21 +0900

    script: replace headless webview to normal webview for fixing browser not working error

--- lib/script/script_webview.dart
+++ lib/script/script_webview.dart
@@ -34,51 +34,6 @@ class _ScriptWebViewState extends State<ScriptWebView>
   void initState() {
     super.initState();
 
-    headlessWebView = HeadlessInAppWebView(
-      initialUrlRequest: URLRequest(
-        url: Uri.parse('https://hitomi.la'),
-      ),
-      onWebViewCreated: (controller) {
-        webViewController = controller;
-
-        controller.addJavaScriptHandler(
-            handlerName: 'gg',
-            callback: (args) {
-              gg_m = args[0];
-              gg_b = args[1];
-
-              return {};
-            });
-      },
-      onLoadStart: (controller, url) async {
-        Future.delayed(const Duration(milliseconds: 300))
-            .then((value) => controller.stopLoading());
-      },
-      onLoadStop: (controller, url) async {
-        await controller.evaluateJavascript(source: '''
-          var r = "";
-          for (var i = 0; i < 4096; i++) {
-            r += gg.m(i).toString();
-            r += ",";
-          }
-          console.log(gg);
-          window.flutter_inappwebview.callHandler('gg', ...[r, gg.b]);
-          ''');
-
-        if (gg_m == null || !(gg_m!.startsWith('0') || gg_m!.startsWith('1'))) {
-          Logger.error(
-              '[Script Webview] Update Fail!\ngg_m: $gg_m\ngg_b: $gg_b');
-          return;
-        }
-
-        Logger.info('[Script Webview] Update Sync!');
-
-        await ScriptManager.setV4(gg_m!, gg_b!);
-      },
-    );
-
-    headlessWebView.run();
-
     timer = Timer.periodic(const Duration(minutes: 1), timerCallback);
   }
 
@@ -89,6 +44,55 @@ class _ScriptWebViewState extends State<ScriptWebView>
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return const SizedBox.shrink();
+    return Visibility(
+      visible: false,
+      maintainState: true,
+      child: SizedBox(
+        height: 1,
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: Uri.parse('https://hitomi.la'),
+          ),
+          onWebViewCreated: (controller) {
+            webViewController = controller;
+
+            controller.addJavaScriptHandler(
+                handlerName: 'gg',
+                callback: (args) {
+                  gg_m = args[0];
+                  gg_b = args[1];
+
+                  return {};
+                });
+          },
+          onLoadStart: (controller, url) async {
+            Future.delayed(const Duration(milliseconds: 300))
+                .then((value) => controller.stopLoading());
+          },
+          onLoadStop: (controller, url) async {
+            await controller.evaluateJavascript(source: '''
+          var r = "";
+          for (var i = 0; i < 4096; i++) {
+            r += gg.m(i).toString();
+            r += ",";
+          }
+          console.log(gg);
+          window.flutter_inappwebview.callHandler('gg', ...[r, gg.b]);
+          ''');
+
+            if (gg_m == null ||
+                !(gg_m!.startsWith('0') || gg_m!.startsWith('1'))) {
+              Logger.error(
+                  '[Script Webview] Update Fail!\ngg_m: $gg_m\ngg_b: $gg_b');
+              return;
+            }
+
+            Logger.info('[Script Webview] Update Sync!');
+
+            await ScriptManager.setV4(gg_m!, gg_b!);
+          },
+        ),
+      ),
+    );
   }
 }
