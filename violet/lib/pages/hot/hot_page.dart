74a1f1086aaf2c5d6263264670a477060ee39555
violet-dev <violet.dev.master@gmail.com>
2022-10-02 10:34:52 +0900

    theme: apply should reload

--- violet/lib/pages/hot/hot_page.dart
+++ violet/lib/pages/hot/hot_page.dart
@@ -18,6 +18,7 @@ import 'package:violet/server/violet.dart';
 import 'package:violet/settings/settings.dart';
 import 'package:violet/widgets/article_item/article_list_item_widget.dart';
 import 'package:violet/widgets/search_bar.dart';
+import 'package:violet/widgets/theme_switchable_state.dart';
 
 class HotPage extends StatefulWidget {
   const HotPage();
@@ -28,9 +29,16 @@ class HotPage extends StatefulWidget {
 
 typedef RequestType = Tuple2<int, List<Tuple2<QueryResult, int>>?>;
 
-class _HotPageState extends State<HotPage> with AutomaticKeepAliveClientMixin {
+class _HotPageState extends ThemeSwitchableState<HotPage>
+    with AutomaticKeepAliveClientMixin<HotPage> {
   @override
   bool get wantKeepAlive => true;
+
+  @override
+  VoidCallback? get shouldReloadCallback => () => _shouldReload = true;
+
+  bool _shouldReload = false;
+  final Map<String, GlobalKey> itemKeys = {};
 
   AsyncMemoizer<RequestType>? _memoizer;
   Future<RequestType>? future;
@@ -47,109 +55,119 @@ class _HotPageState extends State<HotPage> with AutomaticKeepAliveClientMixin {
 
     future ??= _request();
 
-    cachedListView ??= FutureBuilder(
-      future: future,
-      builder: (context, AsyncSnapshot<RequestType> snapshot) {
-        late Widget sliverList;
+    if (cachedListView == null || _shouldReload) {
+      _shouldReload = false;
 
-        if (!snapshot.hasData) {
-          sliverList = const SliverToBoxAdapter(
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        } else if (snapshot.data!.item1 != 200) {
-          final errmsg = {
-            '400': 'Bad Request',
-            '403': 'Forbidden',
-            '429': 'Too Many Requests',
-            '500': 'Internal Server Error',
-            '502': 'Bad Gateway',
-            '521': 'Web server is down',
-            //
-            '900': 'Nothing To Display',
-            '901': 'No Query Results.',
-          };
+      itemKeys.clear();
 
-          sliverList = SliverToBoxAdapter(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 150,
-                    height: 150,
-                    child: Lottie.asset(
-                      'assets/lottie/3227-error-404-facebook-style.json',
-                    ),
-                  ),
-                  Container(height: 16),
-                  const Text(
-                    'Oops! Something was wrong!',
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  Container(height: 2),
-                  const Text(
-                    'If you still encounter this error, please contact the developer!',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Text(
-                      'Error${errmsg[snapshot.data!.item1] != null ? ': ${errmsg[snapshot.data!.item1]!}' : ' Code: ${snapshot.data!.item1}'}')
-                ],
+      cachedListView = FutureBuilder(
+        future: future,
+        builder: (context, AsyncSnapshot<RequestType> snapshot) {
+          late Widget sliverList;
+
+          if (!snapshot.hasData) {
+            sliverList = const SliverToBoxAdapter(
+              child: Center(
+                child: CircularProgressIndicator(),
               ),
-            ),
-          );
-        } else {
-          final windowWidth = MediaQuery.of(context).size.width;
+            );
+          } else if (snapshot.data!.item1 != 200) {
+            final errmsg = {
+              '400': 'Bad Request',
+              '403': 'Forbidden',
+              '429': 'Too Many Requests',
+              '500': 'Internal Server Error',
+              '502': 'Bad Gateway',
+              '521': 'Web server is down',
+              //
+              '900': 'Nothing To Display',
+              '901': 'No Query Results.',
+            };
 
-          final results = snapshot.data!.item2!;
-
-          sliverList = SliverList(
-            delegate: SliverChildListDelegate(
-              results.map((x) {
-                return Align(
-                  key: Key('views$index/${x.item1.id()}'),
-                  alignment: Alignment.center,
-                  child: Provider<ArticleListItem>.value(
-                    value: ArticleListItem.fromArticleListItem(
-                      queryResult: x.item1,
-                      showDetail: true,
-                      showUltra: true,
-                      addBottomPadding: true,
-                      width: (windowWidth - 4.0),
-                      thumbnailTag: const Uuid().v4(),
-                      viewed: x.item2,
-                      usableTabList: results.map((e) => e.item1).toList(),
+            sliverList = SliverToBoxAdapter(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 150,
+                      height: 150,
+                      child: Lottie.asset(
+                        'assets/lottie/3227-error-404-facebook-style.json',
+                      ),
                     ),
-                    child: const ArticleListItemWidget(),
-                  ),
-                );
-              }).toList(),
-            ),
-          );
-        }
-
-        return CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: <Widget>[
-            SliverPersistentHeader(
-              floating: true,
-              delegate: AnimatedOpacitySliver(
-                searchBar: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Stack(children: <Widget>[
-                      _filter(),
-                      _title(),
-                    ])),
+                    Container(height: 16),
+                    const Text(
+                      'Oops! Something was wrong!',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    Container(height: 2),
+                    const Text(
+                      'If you still encounter this error, please contact the developer!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                        'Error${errmsg[snapshot.data!.item1] != null ? ': ${errmsg[snapshot.data!.item1]!}' : ' Code: ${snapshot.data!.item1}'}')
+                  ],
+                ),
               ),
-            ),
-            sliverList,
-          ],
-        );
-      },
-    );
+            );
+          } else {
+            final windowWidth = MediaQuery.of(context).size.width;
+
+            final results = snapshot.data!.item2!;
+
+            sliverList = SliverList(
+              delegate: SliverChildListDelegate(
+                results.map((x) {
+                  final keyStr = 'views$index/${x.item1.id()}';
+                  if (!itemKeys.containsKey(keyStr)) {
+                    itemKeys[keyStr] = GlobalKey();
+                  }
+                  return Align(
+                    key: itemKeys[keyStr],
+                    alignment: Alignment.center,
+                    child: Provider<ArticleListItem>.value(
+                      value: ArticleListItem.fromArticleListItem(
+                        queryResult: x.item1,
+                        showDetail: true,
+                        showUltra: true,
+                        addBottomPadding: true,
+                        width: (windowWidth - 4.0),
+                        thumbnailTag: const Uuid().v4(),
+                        viewed: x.item2,
+                        usableTabList: results.map((e) => e.item1).toList(),
+                      ),
+                      child: const ArticleListItemWidget(),
+                    ),
+                  );
+                }).toList(),
+              ),
+            );
+          }
+
+          return CustomScrollView(
+            physics: const BouncingScrollPhysics(),
+            slivers: <Widget>[
+              SliverPersistentHeader(
+                floating: true,
+                delegate: AnimatedOpacitySliver(
+                  searchBar: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Stack(children: <Widget>[
+                        _filter(),
+                        _title(),
+                      ])),
+                ),
+              ),
+              sliverList,
+            ],
+          );
+        },
+      );
+    }
 
     return Scaffold(
       body: SafeArea(
