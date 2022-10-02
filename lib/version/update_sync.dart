402256e2bcb24cdcfff4bf5ec05e89c49d9292a2
violet-dev <violet.dev.master@gmail.com>
2022-10-02 16:14:01 +0900

    version: fix version 1.28.0

--- lib/version/update_sync.dart
+++ lib/version/update_sync.dart
@@ -14,7 +14,7 @@ class UpdateSyncManager {
 
   // Current version
   static const int majorVersion = 1;
-  static const int minorVersion = 27;
+  static const int minorVersion = 28;
   static const int patchVersion = 0;
 
   static String get currentVersion =>
