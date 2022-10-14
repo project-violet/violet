e6f59b80fba3e8302941bb04b2345a8b3f57997f
violet-dev <violet.dev.master@gmail.com>
2022-10-14 09:52:34 +0900

    update .gitignore

--- emulator.bat
+++
@@ -1,2 +0,0 @@
-C:\Users\rollrat\AppData\Local\Android\Sdk\emulator\emulator.exe -list-avds
-C:\Users\rollrat\AppData\Local\Android\Sdk\emulator\emulator.exe -avd Pixel_5_API_32 -sdcard mySdCardFile.img -logcat '*:v'
