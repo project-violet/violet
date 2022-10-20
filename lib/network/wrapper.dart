32944c5c5f457488d398ff0e670c1bc9ab1e35e5
violet-dev <violet.dev.master@gmail.com>
2022-10-20 16:53:25 +0900

    refactor(network): refactor code

--- lib/network/wrapper.dart
+++ lib/network/wrapper.dart
@@ -23,128 +23,24 @@ class HttpWrapper {
 Future<http.Response> get(String url,
     {Map<String, String>? headers, Duration? timeout}) async {
   if (url.contains('exhentai.org')) {
-    if (HttpWrapper.cacheResponse.containsKey(url)) {
-      return HttpWrapper.cacheResponse[url]!;
-    }
-    await HttpWrapper.throttlerExHentai.acquire();
-    if (HttpWrapper.cacheResponse.containsKey(url)) {
-      HttpWrapper.throttlerExHentai.release();
-      return HttpWrapper.cacheResponse[url]!;
-    }
-    Logger.info('[Http Request] GET: $url');
-    var retry = 0;
-    while (true) {
-      var timeout = false;
-      var res = await http
-          .get(Uri.parse(url), headers: headers)
-          .timeout(Duration(seconds: retry > 3 ? 1000000 : 3), onTimeout: () {
-        timeout = true;
-        return http.Response('', 200);
-      }).catchError((e, st) {
-        Logger.error('[Http Request] GET: $url\n'
-            'E:$e\n'
-            '$st');
-        HttpWrapper.throttlerExHentai.release();
-      });
-      retry++;
-      if (timeout) {
-        if (retry > 3) HttpWrapper.throttlerExHentai.release();
-        Logger.info('[Http Request] GETS: $url, $retry');
-        continue;
-      }
-      if (res.statusCode != 200) {
-        Logger.warning('[Http Response] CODE: ${res.statusCode}, GET: $url');
-      }
-      Logger.info('[Http Request] GETS: $url');
-      if (!HttpWrapper.cacheResponse.containsKey(url) &&
-          res.statusCode == 200) {
-        HttpWrapper.cacheResponse[url] = res;
-      }
-      HttpWrapper.throttlerExHentai.release();
-      return res;
-    }
+    return await _ehentaiGet(url, HttpWrapper.throttlerExHentai,
+        headers: headers, timeout: timeout);
   } else if (url.contains('e-hentai.org')) {
-    if (HttpWrapper.cacheResponse.containsKey(url)) {
-      return HttpWrapper.cacheResponse[url]!;
-    }
-    await HttpWrapper.throttlerEHentai.acquire();
-    if (HttpWrapper.cacheResponse.containsKey(url)) {
-      HttpWrapper.throttlerEHentai.release();
-      return HttpWrapper.cacheResponse[url]!;
-    }
-    Logger.info('[Http Request] GET: $url');
-    var retry = 0;
-    while (true) {
-      var timeout = false;
-      var res = await http
-          .get(Uri.parse(url), headers: headers)
-          .timeout(Duration(seconds: retry > 3 ? 1000000 : 3), onTimeout: () {
-        timeout = true;
-        return http.Response('', 200);
-      }).catchError((e, st) {
-        Logger.error('[Http Request] GET: $url\n'
-            'E:$e\n'
-            '$st');
-        HttpWrapper.throttlerEHentai.release();
-        throw e;
-      });
-      retry++;
-      if (timeout) {
-        if (retry > 3) HttpWrapper.throttlerEHentai.release();
-        Logger.info('[Http Request] GETS: $url, $retry');
-        continue;
-      }
-      if (res.statusCode != 200) {
-        Logger.warning('[Http Response] CODE: ${res.statusCode}, GET: $url');
-      }
-      Logger.info('[Http Request] GETS: $url');
-      if (!HttpWrapper.cacheResponse.containsKey(url) &&
-          res.statusCode == 200) {
-        HttpWrapper.cacheResponse[url] = res;
-      }
-      HttpWrapper.throttlerEHentai.release();
-      return res;
-    }
+    return await _ehentaiGet(url, HttpWrapper.throttlerEHentai,
+        headers: headers, timeout: timeout);
   } else if (url.contains('ltn.hitomi.la') ||
       url.contains(
           'raw.githubusercontent.com/project-violet/violet-message-search')) {
-    Logger.info('[Http Cache] GET: $url');
-    if (HttpWrapper.cacheResponse.containsKey(url)) {
-      return HttpWrapper.cacheResponse[url]!;
-    }
-    Response res;
-    if (timeout == null) {
-      res = await http.get(Uri.parse(url), headers: headers);
-    } else {
-      bool isTimeout = false;
-      var retry = 0;
-      do {
-        isTimeout = false;
-        res = await http.get(Uri.parse(url), headers: headers).timeout(timeout,
-            onTimeout: () {
-          isTimeout = true;
-          retry++;
-          return http.Response('', 200);
-        });
-      } while (isTimeout && retry < 10);
-    }
-    if (res.statusCode != 200) {
-      Logger.warning('[Http Response] CODE: ${res.statusCode}, GET: $url');
-    }
-    if (!HttpWrapper.cacheResponse.containsKey(url) &&
-        res.statusCode == 200 &&
-        res.body.trim().isNotEmpty) {
-      HttpWrapper.cacheResponse[url] = res;
-    }
-    return res;
-  } else {
-    Logger.info('[Http Request] GET: $url');
-    var res = await http.get(Uri.parse(url), headers: headers);
-    if (res.statusCode != 200) {
-      Logger.warning('[Http Response] CODE: ${res.statusCode}, GET: $url');
-    }
-    return res;
+    return await _scriptGet(url, headers: headers, timeout: timeout);
   }
+
+  Logger.info('[Http Request] GET: $url');
+  final res = await http.get(Uri.parse(url), headers: headers);
+  if (res.statusCode != 200) {
+    Logger.warning('[Http Response] CODE: ${res.statusCode}, GET: $url');
+  }
+
+  return res;
 }
 
 Future<http.Response> post(String url,
@@ -157,5 +53,99 @@ Future<http.Response> post(String url,
   if (res.statusCode != 200) {
     Logger.warning('[Http Response] CODE: ${res.statusCode}, POST: $url');
   }
+  return res;
+}
+
+Future<http.Response> _ehentaiGet(String url, Semaphore throttler,
+    {Map<String, String>? headers, Duration? timeout}) async {
+  if (HttpWrapper.cacheResponse.containsKey(url)) {
+    return HttpWrapper.cacheResponse[url]!;
+  }
+
+  await throttler.acquire();
+
+  if (HttpWrapper.cacheResponse.containsKey(url)) {
+    throttler.release();
+    return HttpWrapper.cacheResponse[url]!;
+  }
+
+  Logger.info('[Http Request] GET: $url');
+  var retry = 0;
+  while (true) {
+    var timeout = false;
+
+    final res = await http.get(Uri.parse(url), headers: headers).timeout(
+      Duration(seconds: retry > 3 ? 1000000 : 3),
+      onTimeout: () {
+        timeout = true;
+        return http.Response('', 200);
+      },
+    ).catchError((e, st) {
+      Logger.error('[Http Request] GET: $url\n'
+          'E:$e\n'
+          '$st');
+      throttler.release();
+      throw e;
+    });
+
+    retry++;
+
+    if (timeout) {
+      if (retry > 3) throttler.release();
+      Logger.info('[Http Request] GETS: $url, $retry');
+      continue;
+    }
+
+    if (res.statusCode != 200) {
+      Logger.warning('[Http Response] CODE: ${res.statusCode}, GET: $url');
+    }
+
+    Logger.info('[Http Request] GETS: $url');
+    if (!HttpWrapper.cacheResponse.containsKey(url) && res.statusCode == 200) {
+      HttpWrapper.cacheResponse[url] = res;
+    }
+
+    throttler.release();
+    return res;
+  }
+}
+
+Future<http.Response> _scriptGet(String url,
+    {Map<String, String>? headers, Duration? timeout}) async {
+  Logger.info('[Http Cache] GET: $url');
+
+  if (HttpWrapper.cacheResponse.containsKey(url)) {
+    return HttpWrapper.cacheResponse[url]!;
+  }
+
+  Response res;
+  if (timeout == null) {
+    res = await http.get(Uri.parse(url), headers: headers);
+  } else {
+    bool isTimeout = false;
+    var retry = 0;
+    do {
+      isTimeout = false;
+      res = await http.get(Uri.parse(url), headers: headers).timeout(
+        timeout,
+        onTimeout: () {
+          isTimeout = true;
+          retry++;
+          return http.Response('', 200);
+        },
+      );
+    } while (isTimeout && retry < 10);
+  }
+
+  if (res.statusCode != 200) {
+    Logger.warning('[Http Response] CODE: ${res.statusCode}, GET: $url');
+  }
+
+  if (!HttpWrapper.cacheResponse.containsKey(url) &&
+      res.statusCode == 200 &&
+      res.body.trim().isNotEmpty) {
+    HttpWrapper.cacheResponse[url] = res;
+  }
+
   return res;
 }
