2a69ac4c90c180a81bec64f4d3b98d1dec482e14
violet-dev <violet.dev.master@gmail.com>
2022-10-20 16:13:57 +0900

    script: show sync fail message

--- violet/lib/script/script_webview.dart
+++ violet/lib/script/script_webview.dart
@@ -6,7 +6,9 @@ import 'dart:async';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:violet/log/log.dart';
+import 'package:violet/other/dialogs.dart';
 import 'package:violet/script/script_manager.dart';
+import 'package:violet/settings/settings.dart';
 
 class ScriptWebViewProxy {
   static VoidCallback? reload;
@@ -38,11 +40,26 @@ class _ScriptWebViewState extends State<ScriptWebView>
 
     timer = Timer.periodic(const Duration(minutes: 1), timerCallback);
 
+    // run script readiness probe
+    Future.delayed(const Duration(seconds: 10))
+        .then((value) => v4FailCheckProbe());
+
     ScriptWebViewProxy.reload = () => webViewController?.reload();
   }
 
   Future<void> timerCallback(timer) async {
     webViewController?.reload();
+  }
+
+  Future<void> v4FailCheckProbe() async {
+    if (ScriptManager.enableV4) return;
+    if (Settings.routingRule.first != 'Hitomi' &&
+        Settings.routingRule.first != 'Hiyobi') return;
+
+    showOkDialog(
+        context,
+        '내부 스크립트 동기화 로직을 실행할 수 없습니다. https://hitomi.la 에서 정상적인 응답을 받을 수 없었습니다. '
+        'VPN이나 DPI Bypass Tool을 사용해주시고, 그래도 이 메시지가 계속 표시된다면 개발자에게 알려주세요.');
   }
 
   @override
