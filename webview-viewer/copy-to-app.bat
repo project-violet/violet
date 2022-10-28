7cbc891d8e4358d8aa8f281efa3ff51d2058620e
violet-dev <violet.dev.master@gmail.com>
2022-10-28 16:33:21 +0900

    webview: add copy to app script

---
+++ webview-viewer/copy-to-app.bat
@@ -0,0 +1,8 @@
+:: This source code is a part of Project Violet.
+:: Copyright (C) 2020-2022. violet-team. Licensed under the Apache-2.0 License.
+
+:: copy build webview to assets subdirectory
+
+rmdir ..\assets\webview /S /Q
+mkdir ..\assets\webview
+xcopy build ..\assets\webview /E/H
