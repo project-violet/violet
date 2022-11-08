ae01a63cb45de9e84896ce487d4b1d53b0e815eb
Jiho Lee <optional.int@kakao.com>
2022-11-08 12:34:15 +0900

    webview: use lint

--- webview-viewer/src/styles/index.ts
+++ webview-viewer/src/styles/index.ts
@@ -1,8 +1,8 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const MyImage = styled.img`
-  width: 100%;
-  padding: 0;
-  margin: 0;
-  display: block;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+    display: block;
 `;
