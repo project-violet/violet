1b96ce60826dd50b05184ec20d1938209d1a278c
Jiho Lee <optional.int@kakao.com>
2022-11-08 12:34:15 +0900

    webview: use lint

--- violet/webview-viewer/src/components/image.tsx
+++ violet/webview-viewer/src/components/image.tsx
@@ -1,21 +1,16 @@
-import { useRef, useState } from "react";
-import { IImageProps } from "../interfaces/image";
-import { MyImage } from "../styles";
+import { useRef, useState } from 'react';
+import { IImageProps } from '../interfaces/image';
+import { MyImage } from '../styles';
 
 export function Image({ src }: IImageProps) {
-  const [minHeight, setMinHeight] = useState(300);
-  const imgRef = useRef<HTMLImageElement>(null);
+    const [minHeight, setMinHeight] = useState(0);
+    const imgRef = useRef<HTMLImageElement>(null);
 
-  function onLoad() {
-    setMinHeight(0);
-  }
-
-  return (
-    <MyImage
-      ref={imgRef}
-      src={src}
-      onLoad={onLoad}
-      style={{ minHeight: `${minHeight}px` }}
-    />
-  );
+    return (
+        <MyImage
+            ref={imgRef}
+            src={src}
+            style={{ minHeight: `${minHeight}px` }}
+        />
+    );
 }
--- violet/webview-viewer/src/utils/image-set.ts
+++ violet/webview-viewer/src/utils/image-set.ts
@@ -1,15 +1,15 @@
-import { IImageSet } from "../interfaces/image-set";
+import { IImageSet } from '../interfaces/image-set';
 
 export function hasImageSet(obj: unknown): obj is IImageSet {
-  return (obj as unknown as IImageSet).imageset !== undefined;
+    return (obj as unknown as IImageSet).imageset !== undefined;
 }
 
 export function getImageSet(): string[] {
-  const e = globalThis;
+    const e = globalThis;
 
-  if (hasImageSet(e)) {
-    return e.imageset;
-  }
+    if (hasImageSet(e)) {
+        return e.imageset;
+    }
 
-  return "123456".split('').map(e => `/test-article/${e}.webp`);
+    return '123456'.split('').map((e) => `/test-article/${e}.webp`);
 }
