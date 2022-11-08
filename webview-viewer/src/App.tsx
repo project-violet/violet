b96b8f308e9c910ae4ac1754ac93fecc2d4820cb
Jiho Lee <optional.int@kakao.com>
2022-11-08 12:31:49 +0900

    feat: add jest dependency

--- webview-viewer/src/App.tsx
+++ webview-viewer/src/App.tsx
@@ -1,15 +1,15 @@
-import "./App.css";
+import './App.css';
 import { Image } from './components/image';
-import { getImageSet } from "./utils/image-set";
+import { getImageSet } from './utils/image-set';
 
 export default function App() {
-  const images = getImageSet();
+    const images = getImageSet();
 
-  return (
-    <div>
-      {images.map((e) => (
-        <Image src={e} />
-      ))}
-    </div>
-  );
+    return (
+        <div>
+            {images.map((e) => (
+                <Image key={e} src={e} />
+            ))}
+        </div>
+    );
 }
