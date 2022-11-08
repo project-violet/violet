a30dfd60e41d29d9dec377dc960ce83753c57bcb
Jiho Lee <optional.int@kakao.com>
2022-11-08 12:44:14 +0900

    webview: delete unused absolute resolve

--- violet/webview-viewer/vite.config.cjs
+++ violet/webview-viewer/vite.config.cjs
@@ -3,11 +3,6 @@ const react = require('@vitejs/plugin-react');
 const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
-    },
-  },
   plugins: [react()],
   server: {
     port: 3000,
