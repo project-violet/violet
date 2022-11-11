8894d9a23734488405e53dd72ad258e391dca255
violet-dev <violet.dev.master@gmail.com>
2022-11-11 11:35:42 +0900

    webview: update image style

--- webview-viewer/src/components/image.tsx
+++ webview-viewer/src/components/image.tsx
@@ -1,9 +1,16 @@
 import { IImageProps, IOnImageError } from '../interfaces/image';
+import styled from 'styled-components';
+
+const MyImage = styled.img`
+    width: 100%;
+    padding: 0;
+    margin: 0;
+    display: block;
+`;
 
 export function Image({ src }: IImageProps) {
     return (
-        <img
-            className="h-[15rem]"
+        <MyImage
             alt="Violet Sample Image"
             src={src}
             onError={({ currentTarget }: IOnImageError) => {
