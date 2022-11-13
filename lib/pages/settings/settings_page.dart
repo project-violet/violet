5f64390bacf74f98222ee0d9f2f3a5e26d0ec273
violet-dev <violet.dev.master@gmail.com>
2022-11-13 23:41:38 +0900

    ios: apply return to top

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -193,6 +193,7 @@ class _SettingsPageState extends State<SettingsPage>
     return SingleChildScrollView(
       padding: EdgeInsets.only(top: statusBarHeight),
       physics: const BouncingScrollPhysics(),
+      controller: PrimaryScrollController.of(context) ?? ScrollController(),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: _cachedGroups!,
