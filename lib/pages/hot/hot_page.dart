db8ba3be92b82a7e396a259109dee024dddd31bc
violet-dev <violet.dev.master@gmail.com>
2022-11-14 11:29:19 +0900

    Revert Revert ios: apply return to top

--- lib/pages/hot/hot_page.dart
+++ lib/pages/hot/hot_page.dart
@@ -148,6 +148,8 @@ class _HotPageState extends ThemeSwitchableState<HotPage>
 
           return CustomScrollView(
             physics: const BouncingScrollPhysics(),
+            controller:
+                PrimaryScrollController.of(context) ?? ScrollController(),
             slivers: <Widget>[
               SliverPersistentHeader(
                 floating: true,
