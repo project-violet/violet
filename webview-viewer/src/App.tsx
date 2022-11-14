4a19e3cad613b133464dfca8aa6a92f01f54f788
Jiho Lee <optional.int@kakao.com>
2022-11-14 17:45:05 +0900

    webview: make image pure memoized - remove chattering

--- webview-viewer/src/App.tsx
+++ webview-viewer/src/App.tsx
@@ -1,9 +1,14 @@
+import React, { useMemo } from 'react';
 import './App.css';
 import { Image } from './components/image';
+import { IImageSet } from './interfaces/image-set';
 import { getImageSet } from './utils/image-set';
 
 export default function App() {
-    const images = getImageSet();
+    const images = useMemo(
+        () => getImageSet(),
+        [(globalThis as unknown as IImageSet)?.imageset]
+    );
 
     return (
         <div>
