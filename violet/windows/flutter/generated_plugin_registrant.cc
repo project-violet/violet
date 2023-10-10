ff21c722fd280d682f013da345b8a9bae02cd084
SliverAppBar <sliverappbar@gmail.com>
2023-10-10 22:24:00 +0900

    build: update dependencies (minor version)

--- violet/windows/flutter/generated_plugin_registrant.cc
+++ violet/windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,15 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <firebase_core/firebase_core_plugin_c_api.h>
 #include <flutter_js/flutter_js_plugin.h>
 #include <local_auth_windows/local_auth_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FirebaseCorePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
   FlutterJsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterJsPlugin"));
   LocalAuthPluginRegisterWithRegistrar(
