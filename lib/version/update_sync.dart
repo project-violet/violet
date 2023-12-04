e5072c7b2328257de62f2cabbed4afe8dc467c93
violet-dev <violet.dev.master@gmail.com>
2023-12-04 12:33:39 +0900

    version: Update version (1.29.2)

--- lib/version/update_sync.dart
+++ lib/version/update_sync.dart
@@ -15,7 +15,7 @@ class UpdateSyncManager {
   // Current version
   static const int majorVersion = 1;
   static const int minorVersion = 29;
-  static const int patchVersion = 1;
+  static const int patchVersion = 2;
 
   static String get currentVersion =>
       '$majorVersion.$minorVersion.$patchVersion';
