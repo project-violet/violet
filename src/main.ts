594c8ae681cfbb3f6975c7b8c783b037483ca4fa
violet-dev <violet.dev.master@gmail.com>
2023-12-25 13:12:54 +0900

    Add cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './common/utils/swagger';
 import { logger } from './common/utils/logger';
+import * as cookies from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
   app.useLogger(logger);
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.use(cookies());
 
   setupSwagger(app);
 
