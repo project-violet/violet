bfa34d7c3c1701bc3c44f79e1df4d4d7116da630
violet-dev <violet.dev.master@gmail.com>
2023-12-25 17:30:04 +0900

    Fix spec

--- violet-server/src/user/user.controller.spec.ts
+++ violet-server/src/user/user.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserController } from './UserController';
+import { UserController } from './user.controller';
 
 describe('UserController', () => {
   let controller: UserController;
