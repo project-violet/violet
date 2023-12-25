80cdeb2cd4d3c1aa5c3b92039259ef93d2b6b6b1
koromo <66541802+violet-dev@users.noreply.github.com>
2023-12-25 11:02:38 +0900

    Create cd.sh

---
+++ cd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# sync docker
+docker stop scsbackend && docker rm $_
+docker pull violet-dev/violet:latest
+
+# download env-file for docker
+aws s3api get-object --bucket violet-config --key .env.docker.prod .env.docker.prod
+
+# setup docker
+docker create --env-file ./.env.docker.prod \
+                  --name violet \
+                  --network host \
+                  --add-host host.docker.internal:host-gateway \
+                  violet-dev/violet:latest
+
+# download env & copy to container
+aws s3api get-object --bucket scs-config --key .prod.env .prod.env
+docker cp .prod.env violet:/home/node/
+
+# run docker
+docker start violet
+docker rmi $(docker images -q violet-dev/violet)
+
+rm .env.docker.prod
+rm .prod.env
