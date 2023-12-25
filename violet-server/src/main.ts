c13c905de385a3bdda604635771fd4203a48961a
violet-dev <violet.dev.master@gmail.com>
2023-12-25 11:10:44 +0900

    Add swagger

--- violet-server/src/main.ts
+++ violet-server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './common/utils/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
+  setupSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
