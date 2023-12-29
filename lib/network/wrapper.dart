067c82019d852a8e9486d1e01a27f1b2ec9e76d6
TaYaKi71751 <shantian.tw@gmail.com>
2023-12-29 10:06:35 +0900

    Add warning message if banned

--- lib/network/wrapper.dart
+++ lib/network/wrapper.dart
@@ -130,10 +130,14 @@ Future<http.Response> _ehentaiGet(String url,
 
     Logger.info('[Http Request] GETS: $url');
 
-    if (!HttpWrapper.cacheResponse.containsKey(url) && res.statusCode == 200
-       && (res.body.contains('<') && res.body.contains('>')) // THIS CHECKS HTML TAG EXISTS (if banned respone no cache)
-    ) {
-      HttpWrapper.cacheResponse[url] = res;
+    if (!HttpWrapper.cacheResponse.containsKey(url) && res.statusCode == 200) {
+      if(
+       (!res.body.contains('<') && !res.body.contains('>')) // THIS CHECKS HTML TAG EXISTS (if banned respone no cache)
+      ) {
+        Logger.warning('[_ehentaiGet] ${res.body}');
+      } else {
+        HttpWrapper.cacheResponse[url] = res;
+      }
     }
 
     throttler.release();
