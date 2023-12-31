97f639b1e4f4bd61201e96c52a6f1f387b048bb8
TaYaKi71751 <shantian.tw@gmail.com>
2023-12-31 16:19:52 +0900

    Warn when set-cookie contains mystery

--- lib/network/wrapper.dart
+++ lib/network/wrapper.dart
@@ -157,6 +157,9 @@ Future<http.Response> _ehentaiGet(String url,
                   '>')) // THIS CHECKS HTML TAG EXISTS (if banned respone no cache)
           ) {
         Logger.warning('[_ehentaiGet] ${res.body}');
+        if(res.headers['set-cookie']?.contains('mystery') ?? false){
+          Logger.warning('[_ehentaiGet] ${res.headers['set-cookie']}');
+        }
       } else {
         HttpWrapper.cacheResponse[url] = res;
       }
