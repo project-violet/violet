4a8f67965b46989813390f3fec95d7cfc9eafef3
TaYaKi71751 <shantian.tw@gmail.com>
2024-01-02 00:15:28 +0900

    Add `ignoreTimeout` for slow environment

--- lib/network/wrapper.dart
+++ lib/network/wrapper.dart
@@ -7,6 +7,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 import 'package:violet/log/log.dart';
+import 'package:violet/settings/settings.dart';
 import 'package:violet/thread/semaphore.dart';
 
 class HttpWrapper {
@@ -95,13 +96,17 @@ Future<http.Response> _ehentaiGet(String url,
     StreamedResponse response;
 
     try {
-      response = await client.send(request).timeout(
-        const Duration(seconds: 3),
-        onTimeout: () {
-          timeout = true;
-          throw TimeoutException('Timeout error');
-        },
-      );
+      final sent = client.send(request);
+      if (!Settings.ignoreTimeout) {
+        sent.timeout(
+          const Duration(seconds: 3),
+          onTimeout: () {
+            timeout = true;
+            throw TimeoutException('Timeout error');
+          },
+        );
+      }
+      response = await sent;
     } catch (e, st) {
       Logger.error('[Http Request] GET: $url\n'
           'E:$e\n'
@@ -155,14 +160,18 @@ Future<http.Response> _scriptGet(String url,
     var retry = 0;
     do {
       isTimeout = false;
-      res = await http.get(Uri.parse(url), headers: headers).timeout(
-        timeout,
-        onTimeout: () {
-          isTimeout = true;
-          retry++;
-          return http.Response('', 200);
-        },
-      );
+      final sent = http.get(Uri.parse(url), headers: headers);
+      if(!Settings.ignoreTimeout){
+        sent.timeout(
+          timeout,
+          onTimeout: () {
+            isTimeout = true;
+            retry++;
+            return http.Response('', 200);
+          },
+        );
+      }
+      res = await sent;
     } while (isTimeout && retry < 10);
   }
 
