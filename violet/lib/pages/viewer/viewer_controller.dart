c97fd7a6dd0f5645dfe261c7190ff519ad43529a
violet-dev <violet.dev.master@gmail.com>
2024-01-01 09:15:33 +0900

    Revert "ios: Fix viewer full screen error"
    This reverts commit ca2eb69a6901f7f751a5f2bb3c7eb5952b0a72e7.

--- violet/lib/pages/viewer/viewer_controller.dart
+++ violet/lib/pages/viewer/viewer_controller.dart
@@ -1,8 +1,6 @@
 // This source code is a part of Project Violet.
 // Copyright (C) 2020-2023. violet-team. Licensed under the Apache-2.0 License.
 
-import 'dart:io';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -216,14 +214,14 @@ class ViewerController extends GetxController {
     if (!overlay.value) {
       overlay.value = !overlay.value;
       opacity.value = 1.0;
-      if (!Settings.disableFullScreen || Platform.isIOS) {
+      if (!Settings.disableFullScreen) {
         SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
           SystemUiOverlay.top,
           SystemUiOverlay.bottom,
         ]);
       }
     } else {
-      if (!Settings.disableFullScreen || Platform.isIOS) {
+      if (!Settings.disableFullScreen) {
         SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
       }
       opacity.value = 0.0;
