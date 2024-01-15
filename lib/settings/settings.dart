dae9a9a95d3918ec32c79f275c2107c736036e91
TaYaKi71751 <shantian.tw@gmail.com>
2024-01-15 21:21:34 +0900

    Add Settings.searchCategory

--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -57,6 +57,7 @@ class Settings {
   static late List<String> searchRule;
   static late bool searchNetwork;
   static late bool searchExpunged;
+  static late int searchCategory;
 
   // Global? English? Korean?
   static late String databaseType;
@@ -195,6 +196,7 @@ class Settings {
             .split('|');
     searchNetwork = await _getBool('searchnetwork');
     searchExpunged = await _getBool('searchexpunged');
+    searchCategory = await _getInt('searchcategory', 993);
 
     if (!routingRule.contains('Hiyobi')) {
       routingRule.add('Hiyobi');
@@ -691,6 +693,12 @@ class Settings {
     searchExpunged = nn;
 
     await prefs.setBool('searchexpunged', nn);
+  }
+
+  static Future<void> setSearchCategory(int nn) async {
+    searchCategory = nn;
+
+    await prefs.setInt('searchcategory', nn);
   }
 
   static Future<void> setSearchPure(bool nn) async {
