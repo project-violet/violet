5baafa823c5aaed2916e1c77fb14c0386807e606
violet-dev <violet.dev.master@gmail.com>
2024-02-05 23:15:06 +0900

    Clippy

--- lib/pages/viewer/horizontal_viewer_page.dart
+++ lib/pages/viewer/horizontal_viewer_page.dart
@@ -5,7 +5,6 @@ import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:extended_image/extended_image.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:photo_view/photo_view.dart';
