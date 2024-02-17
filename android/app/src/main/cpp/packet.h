7b32e1e99df98b95c45acc61f3ad0a9ac18ddac4
violet-dev <violet.dev.master@gmail.com>
2024-02-17 20:02:34 +0900

    Remove tun2http legacy

--- android/app/src/main/cpp/packet.h
+++
@@ -1,7 +0,0 @@
-#ifndef DPITUNNEL_PACKET_H
-#define DPITUNNEL_PACKET_H
-
-int parse_request(std::string request, std::string & method, std::string & host, int & port);
-void modify_http_request(std::string & request, bool hostlist_condition);
-
-#endif //DPITUNNEL_PACKET_H
