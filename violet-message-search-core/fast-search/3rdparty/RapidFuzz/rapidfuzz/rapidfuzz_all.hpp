8d2ab473c9b4d782deb8ec154ecbdfd88b163d90
violet-dev <violet.dev.master@gmail.com>
2024-06-08 23:00:06 +0900

    Remove RapidFuzz package

--- violet-message-search-core/fast-search/3rdparty/RapidFuzz/rapidfuzz/rapidfuzz_all.hpp
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: MIT */
-/* Copyright © 2021 Max Bachmann */
-
-#pragma once
-#include <rapidfuzz/fuzz.hpp>
-#include <rapidfuzz/string_metric.hpp>
-#include <rapidfuzz/utils.hpp>
