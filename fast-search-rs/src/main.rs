f7eb2aca304ad308914ff462ed48405a2c82832a
violet-dev <violet.dev.master@gmail.com>
2024-08-03 18:32:52 +0900

    Add rocket server

--- fast-search-rs/src/main.rs
+++ fast-search-rs/src/main.rs
@@ -1,10 +1,14 @@
 use std::path::PathBuf;
 
+use lazy_static::lazy_static;
 use message::load_messages;
 use structopt::StructOpt;
 
 mod binding;
 mod message;
+
+#[macro_use]
+extern crate rocket;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "fast-search", about = "Fast Search for Comic Message")]
@@ -14,13 +18,30 @@ struct Opt {
     token: Option<String>,
 
     #[structopt(long, parse(from_os_str), default_value = "./merged.json")]
-    data_path: PathBuf,
+    data_paths: Vec<PathBuf>,
 }
 
-fn main() {
-    let opt = Opt::from_args();
+lazy_static! {
+    static ref OPT: Opt = Opt::from_args();
+}
 
-    let messages = load_messages(opt.data_path);
+#[get("/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
 
-    println!("Server opened at http://{}:{}", opt.host, opt.port);
+#[launch]
+fn rocket() -> _ {
+    OPT.data_paths
+        .iter()
+        .for_each(|path| load_messages(path.clone()));
+
+    rocket::build()
+        .configure(
+            // https://api.rocket.rs/v0.4/rocket/config/
+            rocket::Config::figment()
+                .merge(("address", OPT.host.clone()))
+                .merge(("port", OPT.port)),
+        )
+        .mount("/hello", routes![hello])
 }
