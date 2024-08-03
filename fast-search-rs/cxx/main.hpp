7cb5276fa1a962ce9e27578e67fe50f8c5082cca
violet-dev <violet.dev.master@gmail.com>
2024-08-03 17:23:22 +0900

    Expose CachedPartialRatio::similarity method

--- fast-search-rs/cxx/main.hpp
+++ fast-search-rs/cxx/main.hpp
@@ -19,6 +19,18 @@ extern "C" struct CachedRatioBinding *create(const char *query);
 extern "C" double similarity(struct CachedRatioBinding *binding,
                              const char *message);
 
+struct CachedPartialRatioBinding {
+  rapidfuzz::fuzz::CachedPartialRatio<typename std::string::value_type> inner;
+
+public:
+  CachedPartialRatioBinding(const std::string &query) : inner(query) {}
+};
+
+extern "C" struct CachedPartialRatioBinding *create_partial(const char *query);
+
+extern "C" double similarity_partial(struct CachedPartialRatioBinding *binding,
+                                     const char *message);
+
 } // namespace binding
 
 #endif
