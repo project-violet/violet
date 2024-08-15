fd84e111b3ad614f28b6eb5f0a1531c65d0ecfee
violet-dev <violet.dev.master@gmail.com>
2024-08-15 15:46:50 +0900

    Add comment repository to module provider

--- violet-server/src/comment/comment.module.ts
+++ violet-server/src/comment/comment.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from 'src/comment/entity/comment.entity';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import { CommentRepository } from './comment.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Comment])],
-  providers: [CommentService],
+  providers: [CommentRepository, CommentService],
   controllers: [CommentController],
 })
 export class CommentModule {}
