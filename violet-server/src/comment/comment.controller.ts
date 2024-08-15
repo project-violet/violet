3579c1bb2d8481cf36e60787f68c9119e763ac9e
violet-dev <violet.dev.master@gmail.com>
2024-08-15 17:42:46 +0900

    Update annotation for swagger

--- violet-server/src/comment/comment.controller.ts
+++ violet-server/src/comment/comment.controller.ts
@@ -9,11 +9,12 @@ import {
 import { CommentService } from './comment.service';
 import { HmacAuthGuard } from 'src/auth/guards/hmac.guard';
 import { CommentPostDto } from './dtos/comment-post.dto';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { User } from 'src/user/entity/user.entity';
 import { AccessTokenGuard } from 'src/auth/guards/access-token.guard';
 
+@ApiTags('comment')
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
