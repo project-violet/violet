3345b0252d705c98c2cc8f318d1c1b6cb6007cad
violet-dev <violet.dev.master@gmail.com>
2024-09-01 11:54:00 +0900

    Lint

--- violet/lib/component/hitomi/hitomi.dart
+++ violet/lib/component/hitomi/hitomi.dart
@@ -4,7 +4,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:get/get.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:tuple/tuple.dart';
