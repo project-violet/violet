0eac605d62837cbf59417e2db505522302faab39
violet-dev <violet.dev.master@gmail.com>
2024-09-08 12:52:03 +0900

    Set Index to discordId

--- violet-server/src/user/entity/user.entity.ts
+++ violet-server/src/user/entity/user.entity.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity, OneToMany } from 'typeorm';
+import { Column, Entity, OneToMany, Index } from 'typeorm';
 import { Exclude } from 'class-transformer';
 import { Comment } from 'src/comment/entity/comment.entity';
 
@@ -16,6 +16,7 @@ export class User extends CoreEntity {
   userAppId: string;
 
   @Column({ nullable: true })
+  @Index()
   discordId?: string;
 
   @Column({ nullable: true })
@@ -26,6 +27,7 @@ export class User extends CoreEntity {
 
   @Column({ nullable: true })
   @Exclude()
+  @Index()
   refreshToken?: string;
 
   @OneToMany(() => Comment, (comment) => comment.user)
